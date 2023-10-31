4e7c714b8bb8422075468a60362e5b5aa1ce9b21
Ian Chege <ianchege001@gmail.com>
2023-10-31 17:40:21 +0300

    feat: add course category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { IconBadge } from "@/components/icon-badge"
 import { DescriptionForm } from "./_components/description-form"
 import { TitleForm } from "./_components/title-form"
 import { ImageForm } from "./_components/image-form"
+import { CategoryForm } from "./_components/category-form"
 import { db } from "@/lib/db"
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -19,6 +20,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       id: params.courseId,
     },
   })
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  })
+
   if (!course) {
     return redirect("/")
   }
@@ -55,6 +63,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>
---
+++ scripts/seed.ts
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client")
+
+const database = new PrismaClient()
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Beginner" },
+        { name: "Intermediate" },
+        { name: "Advanced" },
+      ],
+    })
+    console.log("Success")
+  } catch (error) {
+    console.log("Error seeding the DB categories", error)
+  } finally {
+    await database.$disconnect()
+  }
+}
+
+main()
