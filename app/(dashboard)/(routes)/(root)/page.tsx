4fc305256aa82789a69b5f7c6e8734288750a882
Ian Chege <ianchege001@gmail.com>
2023-11-10 11:21:25 +0300

    feat: add user salutation

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import { CheckCircle, Clock } from "lucide-react"
 
@@ -6,10 +6,9 @@ import { getDashboardCourses } from "@/actions/get-dashboard-courses"
 import { CoursesList } from "@/components/courses-list"
 import { InfoCard } from "./_components/info-card"
 
-// import { UserButton, useUser } from "@clerk/nextjs"
-
 export default async function Dashboard() {
   const { userId } = auth()
+  const user = await currentUser()
 
   if (!userId) {
     return redirect("/")
@@ -17,9 +16,16 @@ export default async function Dashboard() {
   const { completedCourses, coursesInProgress } = await getDashboardCourses(
     userId
   )
-  // const { isSignedIn, isLoaded, user } = useUser()
+  // provide a default value for user's name
+  const userName = user?.firstName || "Techie"
   return (
     <div className="p-6 space-y-4">
+      <h1 className="text-green-gray text-4xl font-normal leading-snug">
+        Hi, {userName}
+      </h1>
+      <p className="text-green-gray text-base font-normal">
+        See what&apos;s happening in your classes.
+      </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <InfoCard
           icon={Clock}
@@ -34,11 +40,6 @@ export default async function Dashboard() {
         />
       </div>
       <CoursesList items={[...coursesInProgress, ...completedCourses]} />
-      {/* <UserButton afterSignOutUrl="/" />
-      <h2 className="text-green-gray text-4xl font-normal leading snug">
-        Hi, {isSignedIn ? user.firstName : "Techie"}
-      </h2>
-      <p>See what&apos;s happening in your classes.</p> */}
     </div>
   )
 }
