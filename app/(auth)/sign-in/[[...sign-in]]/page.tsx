3a0a5347005fade8f717d94c8b940c05c3c9488e
Ian Chege <ianchege001@gmail.com>
2023-11-20 14:34:23 +0300

    fix: restore to default

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,18 +2,5 @@ import { SignIn } from "@clerk/nextjs"
 import Image from "next/image"
 
 export default function Page() {
-  return (
-    <div className="flex justify-between gap-4 items-center">
-      <SignIn />
-      <div className="flex justify-end h-screen pl-4 border-l-2 border-solid divide-slate-700">
-        <Image
-          src="/auth.svg"
-          alt=""
-          width={500}
-          height={850}
-          objectFit="fill"
-        />
-      </div>
-    </div>
-  )
+  return <SignIn />
 }
